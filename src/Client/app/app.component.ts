c4c3f35dcd47e02592c10c7404123c2f11b96568
Caleb Wells <calebcwells@gmail.com>
2017-01-11 09:30:32 -0600

    Updated base layout so I don't have to redo it every time I create a new branch.

--- src/Client/app/app.component.ts
+++ src/Client/app/app.component.ts
@@ -4,6 +4,4 @@ import { Component } from '@angular/core';
   selector: 'ng2cli-app',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-    title = 'app works!';
-}
+export class AppComponent { }
